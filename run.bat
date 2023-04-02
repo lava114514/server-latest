bf2a22931ec660b406635ff54887ba0c579f2e8d
lava114514 <2697345722@qq.com>
2023-04-02 12:41:53 +0800

    1

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-java -Xmx2G -jar -javaagent:authlib-injector-1.2.1.jar=https://littleskin.cn/api/yggdrasil purpur-1.18.2-1632.jar nogui
+java -Xmx2G -jar -javaagent:authlib-injector-1.2.1.jar=https://littleskin.cn/api/yggdrasil leaves-1.19.3.jar nogui
 pause
