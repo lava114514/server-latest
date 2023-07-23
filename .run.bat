cf5dcf527584c7437edca61a7c367c9fc6c689f3
lava114514 <2697345722@qq.com>
2023-07-23 21:38:41 +0800

    h2

--- .run.bat
+++
@@ -1,2 +0,0 @@
-java -Xmx2G -javaagent:paperShelled-1.0.0.jar -jar leaves-1.19.4.jar nogui
-pause
---
+++ run.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Java\jdk-20\bin\java.exe" -jar leaves-1.20.1.jar nogui
+pause
+exit
